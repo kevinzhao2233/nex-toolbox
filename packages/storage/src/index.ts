476dafd86c74fe26878ec323f01d72c12b8589db
赵凯文 <kevin_2233@qq.com>
2023-12-15 10:48:50 +0800

    feat(storage): 新增 storage 模块，也是一个封装 localstorage 的工具

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,7 +1,19 @@
-export const log = () => {
-  console.log('hello world');
-};
+import UStorage, { type StorageConfig, type StorageOption } from './Storage';
 
-export const storage = {
-  a: 1,
+export default UStorage;
+
+const globalStorage = new UStorage({ driver: window.localStorage });
+// 设置全局缓存配置
+export const setStorageConfig = (config: StorageOption) => {
+  globalStorage.config(config);
 };
+// 设置缓存
+export const setStorage = (key: string, data: any, config?: StorageConfig) => {
+  globalStorage.set(key, data, config);
+};
+// 获取缓存
+export const getStorage = <T = any>(key: string) => globalStorage.get<T>(key);
+// 移除缓存
+export const removeStorage = (...keys: string[]) => globalStorage.remove(...keys);
+// 清楚缓存
+export const clearStorage = () => globalStorage.clear();
