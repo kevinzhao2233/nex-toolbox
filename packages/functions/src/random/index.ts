f5ce0b31bfe8366c0b5987aa8a6e708e41c8724d
赵凯文 <kevin_2233@qq.com>
2024-02-02 17:04:20 +0800

    feat(functions): random 可以返回小数

--- packages/functions/src/random/index.ts
+++ packages/functions/src/random/index.ts
@@ -2,5 +2,11 @@
  * 生成随机数
  * @param min 最小值
  * @param max 最大值
+ * @param returnDecimal 是否返回小数
  */
-export const random = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+export const random = (min: number, max: number, returnDecimal: boolean = false) => {
+  if (!returnDecimal) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return Math.random() * (max - min) + min;
+};
